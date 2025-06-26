679ddc82609006a7ba95eb6bec28c993782de925
Gonzalo <gonzalo.romanreyes@gmail.com>
2025-06-26 18:42:16 -0400

    primer commit de la pulga store

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+
+function mostrarFecha() {
+
+    let fecha = new Date();
+    let dias = ["Domningo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];  
+
+    const diaSemana = dias[fecha.getDay()];
+    const dia = fecha.getDate();
+    const mes = meses[fecha.getMonth()];
+    const anio = fecha.getFullYear();
+    const horas = fecha.getHours();
+    const minutos = fecha.getMinutes();
+    const segundos = fecha.getSeconds();
+
+    let mensaje = `Hoy es ${diaSemana} ${dia} de ${mes} de ${anio}, y son las ${horas} horas, ${minutos} minutos con ${segundos} segundos.`;
+    document.querySelector("#timer").innerText = mensaje;
+
+}
+
+document.addEventListener("DOMContentLoaded", () => { mostrarFecha();});
+
+Swal.fire("Formulario en mantención!");
